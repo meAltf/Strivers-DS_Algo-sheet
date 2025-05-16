b8f674b0dc49cfac67f2f44ccb1961a2d67fc61f
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-16 22:29:22 +0530

    Brute force : Rotate Array.java

---
+++ Arrays/Medium/Rotate Array.java
@@ -0,0 +1,45 @@
+Problem: Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+  
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+Brute force : 
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n; // handle case where k>n
+        int[] temp = new int[n];
+
+        // placing last k elements of the original array at the beginning of the temp array.
+        for(int i=0; i<k; i++){
+            temp[i] = nums[n-k+i];
+        }
+
+        // placing remaining elements from start to end of the original array after the last K elements.
+        for(int i=0; i<n-k; i++){
+            temp[k+i] = nums[i];
+        }
+
+        // Now copy temp array to nums
+        for(int i=0; i<n; i++){
+            nums[i] = temp[i];
+        }
+    }
+}

932a4e7fd81b14cffc26bb9a85a1877470bb7004
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-17 18:08:11 +0530

    Optimal Approach : Missing Number.java

--- Arrays/Easy/Missing Number.java
+++ Arrays/Easy/Missing Number.java
@@ -46,3 +46,17 @@ class Solution {
         return -1;
     }
 }
+
+
+2. Optimal Approach-
+
+ class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = (n*(n+1))/2;
+        int actualSum = 0;
+        for(int num : nums) actualSum+=num;
+
+        return expectedSum - actualSum;
+    }
+}

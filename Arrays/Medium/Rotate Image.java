332c626117d139a7fb4a5da5a38ae2447c041cf1
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-22 22:50:03 +0530

    Optimal approach : Rotate Image.java

--- Arrays/Medium/Rotate Image.java
+++ Arrays/Medium/Rotate Image.java
@@ -32,3 +32,33 @@ Solution :
 
         return ans;
     }
+
+
+2. Optimal approach : in-place 
+
+ class Solution {
+
+    public void rotate(int[][] matrix) {
+        // 1. transpose matrix
+        for (int i = 0; i < matrix.length; i++) {
+           // To avoid swapping the same elements twice or touching the diagonal unnecessarily, j starts from i.
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // reverse each row
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0, right = matrix.length-1;
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}

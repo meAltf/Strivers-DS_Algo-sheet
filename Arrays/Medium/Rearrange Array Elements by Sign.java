ea4a3e8daee68fb06ba44f0426b36eb5a8ab1217
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-23 16:09:06 +0530

    Optimal approach : Rearrange Array Elements by Sign.java

--- Arrays/Medium/Rearrange Array Elements by Sign.java
+++ Arrays/Medium/Rearrange Array Elements by Sign.java
@@ -73,3 +73,25 @@ Solution :
         return finalArr;
     }
 }
+
+
+2. Optimal approach
+
+ class Solution {
+    public int[] rearrangeArray(int[] nums) {
+      int n = nums.length;
+      int posIndex = 0, negIndex = 1;
+      int[] finalArr = new int[n];
+
+      for(int i=0; i<n; i++){
+        if(nums[i] > 0){
+            finalArr[posIndex] = nums[i];
+            posIndex+=2;
+        }else{
+            finalArr[negIndex] = nums[i];
+            negIndex+=2;
+        }
+      }
+      return finalArr;
+    }
+}

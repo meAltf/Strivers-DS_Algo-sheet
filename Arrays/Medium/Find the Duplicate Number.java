3df0ff1810ee2836bbf7cf4a3de636f8941d16b5
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-23 17:44:17 +0530

    Better approach : Find the Duplicate Number.java

--- Arrays/Medium/Find the Duplicate Number.java
+++ Arrays/Medium/Find the Duplicate Number.java
@@ -40,3 +40,17 @@ class Solution {
         return 0;
     }
 }
+
+2. Better approach 
+
+ class Solution {
+    public int findDuplicate(int[] nums) {
+        // duplicate find krna hai bc
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0; i<n; i++){
+            if(nums[i] == nums[i+1]) return nums[i];
+        }
+        return 0;
+    }
+}

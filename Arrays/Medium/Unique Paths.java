2b8dbf59cc5c2771654f5fde65afe8094c72b09f
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-24 10:41:28 +0530

    Better approach : Dynamic programming - Unique Paths.java

--- Arrays/Medium/Unique Paths.java
+++ Arrays/Medium/Unique Paths.java
@@ -43,3 +43,27 @@ class Solution {
         return countPaths(0,0,m,n);
     }
 }
+
+2. Better approach : Dynamic programming
+
+class Solution {
+    private int countPaths(int i, int j, int m, int n, int[][]dp){
+        // base cases
+        if(i>=m || j>=n) return 0;
+        if(i==m-1 && j==n-1) return 1;
+
+        if(dp[i][j] != 0);
+
+        int right = countPaths(i+1, j, m, n, dp);
+        int down = countPaths(i, j+1, m, n, dp);
+
+        dp[i][j] = right + down;
+
+        return dp[i][j];
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        return countPaths(0,0,m,n,dp);
+    }
+}

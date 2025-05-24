481d287caca38134061e7ebd029396a96dba16d7
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-24 11:00:17 +0530

    Enhancement : Unique Paths.java

--- Arrays/Medium/Unique Paths.java
+++ Arrays/Medium/Unique Paths.java
@@ -52,7 +52,7 @@ class Solution {
         if(i>=m || j>=n) return 0;
         if(i==m-1 && j==n-1) return 1;
 
-        if(dp[i][j] != 0);
+        if(dp[i][j] != 0) return dp[i][j];
 
         int right = countPaths(i+1, j, m, n, dp);
         int down = countPaths(i, j+1, m, n, dp);

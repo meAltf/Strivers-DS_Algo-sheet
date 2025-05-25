54c8b1436761dffaa655111f3b9365bafe998f30
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-25 20:05:54 +0530

    Brute force : optimized | Majority Element II.java

--- Arrays/Hard/Majority Element II.java
+++ Arrays/Hard/Majority Element II.java
@@ -71,3 +71,24 @@ class Solution {
         
     }
 }
+
+3. Brute force : optimized
+
+ class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            if(result.size() == 0 || result.get(0) != nums[i]){
+                int count =0;
+                for(int j = 0; j<n; j++){
+                    if(nums[i] == nums[j]) count++;
+                }
+                if(count > n/3) result.add(nums[i]);
+            }
+            if(result.size() == 2) break;
+        }
+        return result;
+    }
+}

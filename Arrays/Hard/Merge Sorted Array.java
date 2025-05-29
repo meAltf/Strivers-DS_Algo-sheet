f6673ef9c7d9705bab1d8da3a4f6e12919ca45ae
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-05-29 10:01:57 +0530

    Optimal approach : Merge Sorted Array.java

--- Arrays/Hard/Merge Sorted Array.java
+++ Arrays/Hard/Merge Sorted Array.java
@@ -57,3 +57,32 @@ class Solution {
         }
     }
 }
+
+
+2. Optimal Approach :
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        // Optimal Approach :
+        int p1 = m-1;
+        int p2 = n-1;
+        int p = m+n-1;
+
+        while(p1 >= 0 && p2>= 0){
+            if(nums1[p1] > nums2[p2]){
+                nums1[p--] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p--] = nums2[p2];
+                p2--;
+            }
+        }
+
+        // remaining element if nums2 has, bcz nums1 already has correct place
+        while(p2 >= 0){
+            nums1[p--] = nums2[p2];
+            p2--;
+        }
+    }
+}

56754a3c196901b8062be4249a40f2442a86f2a2
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-07 10:30:53 +0530

    Brute force : Longest Subarray with given sum k (positive).java

---
+++ Arrays/Hard/Longest Subarray with given sum k (positive).java
@@ -0,0 +1,40 @@
+Problem Statement: Given an array and a sum k, we need to print the length of the longest subarray that sums to k.
+
+Examples
+
+Example 1:
+Input Format: N = 3, k = 5, array[] = {2,3,5}
+Result: 2
+Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+Example 2:
+Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+Result: 3
+Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+
+Solution :
+
+1. Brute Force : 
+
+private static int longestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        int largest = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int sum = 0;
+                int count = 0;
+                for(int K=i; K<=j; K++){
+                    sum+=nums[K];
+                    count++;
+                }
+
+                if(sum==k){
+                    largest = Math.max(count, largest);
+                }
+            }
+        }
+        return largest;
+    }

6da61ffe3cb8c0a297decab47e4a7bac86d345a6
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-15 23:58:31 +0530

    Optimal approach | Upper Bound.java

--- Binary-search/Easy/Upper Bound.java
+++ Binary-search/Easy/Upper Bound.java
@@ -30,4 +30,24 @@ public int upperBound(int[] nums, int x) {
             if(nums[i] > x) return i;
         }
         return length;
+}
+
+2. Optimal approach :
+
+private int upperBound(int[] nums, int x) {
+        int length = nums.length;
+        int start = 0, end = length-1;
+        int ans = length;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] > x){
+                ans = mid;
+                end = mid-1;
+            } else start = mid+1;
+        }
+        return ans;
     }
+
+

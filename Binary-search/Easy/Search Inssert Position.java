8fa6ea46fa3f849b22d1a0930bcd9a412b12cd58
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-16 01:03:48 +0530

    Optimal approach | Search Insert Position.java

--- Binary-search/Easy/Search Inssert Position.java
+++ Binary-search/Easy/Search Inssert Position.java
@@ -34,3 +34,24 @@ Solution :
         return ans;
     }
 }
+
+2. Optimal approach :
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int ans = nums.length;
+        int start = 0, end = nums.length-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] > target){
+                ans = mid;
+                end = mid-1;
+            } else start = mid+1;
+        }
+        return ans;
+    }
+}

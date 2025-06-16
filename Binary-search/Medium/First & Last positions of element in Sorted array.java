bf3434667d0a7d7292570769f26efc24952834a8
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-16 14:30:28 +0530

    Optimal approach 1 | First & Last positions of element in Sorted array.java

--- Binary-search/Medium/First & Last positions of element in Sorted array.java
+++ Binary-search/Medium/First & Last positions of element in Sorted array.java
@@ -41,3 +41,47 @@ class Solution {
         return ans;
     }
 }
+
+2. Optimal approach | Using lowerBound & upperBound
+
+ class Solution {
+
+    private static int lowerBoundBinary(int[] nums, int x) {
+       int length = nums.length;
+       int start = 0, end = length-1;
+       int ans = length;
+       
+       while(start <= end){
+            int mid = start + (end-start)/2;
+            if (nums[mid] >= x) {
+                ans = mid;
+                end = mid-1;
+            } else start = mid+1;
+       }
+       return ans;
+    }
+
+    private int upperBound(int[] nums, int x) {
+        int length = nums.length;
+        int start = 0, end = length-1;
+        int ans = length;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] > x){
+                ans = mid;
+                end = mid-1;
+            } else start = mid+1;
+        }
+        return ans;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        // edge case
+        int n = nums.length;
+        int lowerB = lowerBoundBinary(nums, target);
+        if(lowerB == n || nums[lowerB] != target) return new int[]{-1,-1};
+        return new int[]{lowerB, upperBound(nums, target)-1};
+    }
+}

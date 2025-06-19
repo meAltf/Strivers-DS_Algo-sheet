dfd1467de5818002ddc47d200199c103c3d9ff29
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-19 23:29:19 +0530

    Update Notes.java

--- Linked-List/Concepts/Notes.java
+++ Linked-List/Concepts/Notes.java
@@ -1,1 +1,8 @@
 1- Basic concepts & implementation
+
+------------------------------------
+1. Not in contigeous location [ randomly placed ]
+2. Able to increase or decrease the size
+3. Examples stack/queue
+4. Real life example- Browser -> nested page we can open & back & forward to close also
+5. 

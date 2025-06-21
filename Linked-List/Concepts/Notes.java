31263c98d7a625cff32d9dc8cdb2ecad2202f76a
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-21 18:56:16 +0530

    regarding DLL | Notes.java

--- Linked-List/Concepts/Notes.java
+++ Linked-List/Concepts/Notes.java
@@ -1,6 +1,8 @@
 1- Basic concepts & implementation
 
-------------------------------------
+-----------------------------------------------------------------------------
+  ----- Singly Linked List ------------
+  
 1. Not in contigeous location [ randomly placed ]
 2. Able to increase or decrease the size
 3. Examples stack/queue
@@ -8,3 +10,13 @@
 5. Node containing two things - >
   1-> Data of the current Node
   2-> Address of the next Node
+
+
+-----------------------------------------------------------------------------
+  ------- Double linked list ---------------
+
+1. Traversing possible from both the end -> head & tails both
+2. Double linked list node containing three things ->
+  1-> Data of the curreent Node
+  2-> Address of the next Node
+  3-> Adress of the previous Node

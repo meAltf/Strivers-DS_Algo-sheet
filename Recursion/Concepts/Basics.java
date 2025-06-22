2674d22974190cac58e8d46a34ee4e28f1304328
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-22 10:34:14 +0530

    Recursion | concept | Basics.java

---
+++ Recursion/Concepts/Basics.java
@@ -0,0 +1,4 @@
+/** Recursion basics **/
+
+1. Recursion : When a function calls itself until a specified condition is met.
+2. Segmentation fault | Stack overFlow : when enumerous function calls waiting in stack without completing their execution called stack overflow.

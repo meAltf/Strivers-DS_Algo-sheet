8233b3a77e47dd68bbd77ce74131796c68a0a451
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-22 11:30:40 +0530

    Backtracking | Basics.java

--- Recursion/Concepts/Basics.java
+++ Recursion/Concepts/Basics.java
@@ -2,3 +2,4 @@
 
 1. Recursion : When a function calls itself until a specified condition is met.
 2. Segmentation fault | Stack overFlow : when enumerous function calls waiting in stack without completing their execution called stack overflow.
+3. Backtracking - Caling function call earlier and then doing print statement or other things.

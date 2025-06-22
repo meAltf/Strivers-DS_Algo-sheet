47b2098299cd33a9d0fdb3aa5a6a400bcc350267
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-22 23:27:15 +0530

    printsubsequencesSumEqualTok |  PrintAllSubsequences.java

--- Recursion/Medium/PrintAllSubsequences.java
+++ Recursion/Medium/PrintAllSubsequences.java
@@ -26,11 +26,34 @@ public class PrintAllSubsequences {
 
     }
 
+    private static void printsubsequencesSumEqualTok(int idx, List<Integer> ds, int sum, int k, int[] arr, int n) {
+        if (idx == n) {
+            if (sum == k) {
+                for (int num : ds) System.out.print(num + " ");
+                System.out.println();
+            }
+            return;
+        }
+
+        // take or pick the particular index into the subsequence | and add it into sum and once removed, minus from sum as well
+        ds.add(arr[idx]);
+        sum += arr[idx];
+        printsubsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n);
+        sum -= arr[idx];
+        ds.remove(ds.size() - 1);
+
+        // not pick, or not take condition, this element is not added to your subsequence
+        printsubsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n);
+    }
+
     public static void main(String[] args) {
-        int[] arr = {3, 1, 2};
+        int[] arr = {1, 2, 1};
         int n = arr.length;
         List<Integer> dsList = new ArrayList<>();
 
         printAllSubsequences(0, dsList, arr, n);
+
+        System.out.println("sum to equal to k : ");
+        printsubsequencesSumEqualTok(0, dsList, 0, k, arr, n);
     }
 }

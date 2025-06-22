cf68633b4d20537be3faa5297e769991f5a6a049
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-23 00:22:59 +0530

    print1subsequencesSumEqualTok | PrintAllSubsequences.java

--- Recursion/Medium/PrintAllSubsequences.java
+++ Recursion/Medium/PrintAllSubsequences.java
@@ -26,7 +26,7 @@ public class PrintAllSubsequences {
 
     }
 
-    private static void printsubsequencesSumEqualTok(int idx, List<Integer> ds, int sum, int k, int[] arr, int n) {
+    private static void printAllsubsequencesSumEqualTok(int idx, List<Integer> ds, int sum, int k, int[] arr, int n) {
         if (idx == n) {
             if (sum == k) {
                 for (int num : ds) System.out.print(num + " ");
@@ -38,22 +38,51 @@ public class PrintAllSubsequences {
         // take or pick the particular index into the subsequence | and add it into sum and once removed, minus from sum as well
         ds.add(arr[idx]);
         sum += arr[idx];
-        printsubsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n);
+        printAllsubsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n);
         sum -= arr[idx];
         ds.remove(ds.size() - 1);
 
         // not pick, or not take condition, this element is not added to your subsequence
-        printsubsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n);
+        printAllsubsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n);
+    }
+
+    private static boolean print1subsequencesSumEqualTok(int idx, List<Integer> ds, int sum, int k, int[] arr, int n) {
+        if (idx == n) {
+            if (sum == k) {
+                for (int num : ds) System.out.print(num + " ");
+                System.out.println();
+                return true;
+            } else return false;
+        }
+
+        // take or pick the particular index into the subsequence | and add it into sum and once removed, minus from sum as well
+        ds.add(arr[idx]);
+        sum += arr[idx];
+        if (print1subsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n) == true) {
+            return true;
+        }
+        sum -= arr[idx];
+        ds.remove(ds.size() - 1);
+
+        // not pick, or not take condition, this element is not added to your subsequence
+        if (print1subsequencesSumEqualTok(idx + 1, ds, sum, k, arr, n) == true) {
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 1};
         int n = arr.length;
+        int k = 2;
         List<Integer> dsList = new ArrayList<>();
 
         printAllSubsequences(0, dsList, arr, n);
 
-        System.out.println("sum to equal to k : ");
-        printsubsequencesSumEqualTok(0, dsList, 0, k, arr, n);
+        System.out.println("All subsequence having sum to equal to k : ");
+        printAllsubsequencesSumEqualTok(0, dsList, 0, k, arr, n);
+
+        System.out.println("print only 1 subsequence : ");
+        print1subsequencesSumEqualTok(0, dsList, 0, k, arr, n);
     }
 }

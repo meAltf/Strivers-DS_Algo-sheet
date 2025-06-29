048ca47293a0c732fce53d10d17543233b452ac1
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2025-06-29 21:58:30 +0530

    GFG | Number of provinces.java

--- Graph/Medium/Number of provinces.java
+++ Graph/Medium/Number of provinces.java
@@ -39,3 +39,33 @@ class Solution {
         return count;
     }
 }
+
+
+2. GFG | Problem statment :
+
+/** Note : Input given as-  List of Lists where each list is a row of 0/1 values  **/
+
+class Solution {
+    static void dfs(int node, boolean[] vis, ArrayList<ArrayList<Integer>> adj, int V){
+        vis[node] = true;
+        for(int i=0; i<V; i++){
+            if(adj.get(node).get(i) == 1 && vis[i] == false){
+                dfs(i, vis, adj, V);
+            }
+        }
+    }
+    
+    
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        boolean[] vis = new boolean[V];
+        int count = 0;
+        for(int i=0; i<V; i++){
+            if(vis[i] == false){
+                count++;
+                dfs(i, vis, adj, V);
+            }
+        }
+        return count;
+    }
+};
